61299c5c63dddc5acd526f5f8fab6c70e9a0b8b7
HayHay102 <smartphone.67156@gmail.com>
2026-04-24 15:10:11 +0700

    Add files via upload

--- plugins/STPhim_plugin.js
+++ plugins/STPhim_plugin.js
@@ -60,8 +60,7 @@ function getUrlList(slug, filtersJson) {
         var page = filters.page || 1;
         var baseUrl = "https://www.sieutamphim.pro";
 
-    // URL rule for typical WP sites
-    if (slug === '' || slug === 'home') {
+    {
         if (page > 1) {
             return baseUrl + "/search/label/" + slug + "/page/" + page + "/";
         }
