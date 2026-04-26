3e7b3ca24aed4bd8593578f538ad65eedb8c0c0d
HayHay102 <smartphone.67156@gmail.com>
2026-04-26 18:11:04 +0700

    Add files via upload

--- pluginstest/test1_plugin.js
+++ pluginstest/test1_plugin.js
@@ -168,6 +168,23 @@ function parseListResponse(html) {
     }
 }
 
+function decodeHtmlEntities(str) {
+    if (!str) return "";
+
+    return str
+        .replace(/&#8211;/g, "-")
+        .replace(/&#8212;/g, "-")
+        .replace(/&#8220;/g, '"')
+        .replace(/&#8221;/g, '"')
+        .replace(/&#8216;/g, "'")
+        .replace(/&#8217;/g, "'")
+        .replace(/&#038;/g, "&")
+        .replace(/&amp;/g, "&")
+        .replace(/&quot;/g, '"')
+        .replace(/&#39;/g, "'")
+        .replace(/&nbsp;/g, " ")
+        .trim();
+}
 function parseSearchResponse(html) {
     return parseListResponse(html);
 }
